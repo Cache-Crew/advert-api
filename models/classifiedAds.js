e2b9fdbbebd039e368565aa5d3e9bdecc974f3e5
Damaris Danso <livingasdamaris@gmail.com>
2025-03-20 16:56:20 +0000

    "added model, controller, routes and validators"

---
+++ models/classifiedAds.js
@@ -0,0 +1,29 @@
+import { Schema, model} from "mongoose";
+import normalize from "normalize-mongoose";
+s
+const classifiedAdsSchema = new Schema({
+    title: { 
+        type: String,
+        required: true,
+    },
+    pictures: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+    },
+}, {
+    timestamps: true
+});
+classifiedAdsSchema.plugin(normalize)
+
+export const AdvertModel = model('ClassifiedAds', classifiedAdsSchema)
